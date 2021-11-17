27ff9270d222e5bc3aff81fca0e0669114ac75a6
Labnan <55809005+Labnann@users.noreply.github.com>
2021-11-18 02:52:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const APOD_API_KEY = process.env.APOD_API_KEY;
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.listen(process.env.port);
+app.listen(process.env.PORT);
 
 app.get('/apod',  (req, res) => {
      axios.get(`https://api.nasa.gov/planetary/apod?api_key=${APOD_API_KEY}`)
